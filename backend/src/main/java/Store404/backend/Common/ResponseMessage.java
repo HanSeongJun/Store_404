0120ccb5c65a33718d69863dfb9dbcc5851b4005
hanseongjun <gkstjdwns2@naver.com>
2025-07-30 16:24:01 +0900

    CREATE 응답 코드, 응답 메시지 생성

---
+++ backend/src/main/java/Store404/backend/Common/ResponseMessage.java
@@ -0,0 +1,25 @@
+package Store404.backend.Common;
+
+public class ResponseMessage {
+
+    // HTTP Status 200
+    String SUCCESS = "Success";
+
+    // HTTP Status 400
+    String VALIDATION_FAILED = "Validation Failed.";
+    String DUPLICATE_EMAIL = "Duplicate Email.";
+    String DUPLICATE_NICKNAME = "Duplicate Nickname.";
+    String DUPLICATE_TEL_NUMBER = "Duplicate Tel Number.";
+    String NOT_EXIST_USER = "This user does not exist.";
+    String NOT_EXIST_BOARD = "This board does not exist.";
+
+    // HTTP Status 401
+    String SIGN_IN_FAIL = "Login Information Mismatch.";
+    String AUTHORIZATION_FAIL = "Authorization Failed.";
+
+    // HTTP Status 403
+    String NO_PERMISSION = "Do not have permission.";
+
+    // HTTP Status 500
+    String DATABASE_ERROR = "Database Error.";
+}
