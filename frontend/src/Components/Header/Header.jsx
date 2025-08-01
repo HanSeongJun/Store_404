447e229df43350fa856362b37eba4cd56d130c2b
hanseongjun <gkstjdwns2@naver.com>
2025-08-01 18:23:41 +0900

    CREATE 헤더 스타일 작성

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,75 +1,81 @@
 import {Link} from "react-router-dom";
+import "./Style.css";
+import logoImage from "../../Assets/logo.png";
 
 function Header() {
 
 
 
     return (
-        <header>
-            <div>
+        <header className="Header">
+            <div className="Header-Container">
                 {/* 로고 */}
-                <Link to="/">
-                    <h1>404</h1>
+                <Link to="/" className="Header-Logo">
+                    <img src={logoImage} alt="404 Logo" className="Header-Logo-Image" />
                 </Link>
 
                 {/* 검색 창 */}
-                <form>
-                    <div>
-                        <input />
-                        <button>검색</button>
+                <form className="Header-Search">
+                    <div className="Header-Search-Container">
+                        <input className="Header-Search-Input" />
+                        <button className="Header-Search-Button">검색</button>
                     </div>
                 </form>
 
                 {/* 사용자 메뉴 */}
-                <div>
+                <div className="Header-User-Menu">
                     <>
-                        <Link to="/">장바구니</Link>
-                        <Link to="/">관리자</Link>
-                        <div>
-                            <button>회원 이름
+                        <Link to="/" className="Header-User-Menu-Link">
+                            장바구니
+                        </Link>
+                        <Link to="/" className="Header-User-Menu-Link">
+                            관리자
+                        </Link>
+                        <div className="Header-User-Menu-Member">
+                            <button className="Header-User-Menu-Button">회원 이름
                                 <span>▼</span>
                             </button>
-                            <div>
-                                <div>
-                                    <div>안녕하세요,</div>
-                                    <div>사용자 님.</div>
+                            <div className="Header-User-Menu-Member-Dropdown">
+                                <div className="Header-User-Menu-Member-Dropdown-Info">
+                                    <div className="Header-User-Menu-Member-Dropdown-Hello">안녕하세요,</div>
+                                    <div className="Header-User-Menu-Member-Dropdown-Member">사용자 님.</div>
                                 </div>
-                                <button>로그아웃</button>
+                                <button className="Header-User-Menu-Logout-Button">로그아웃</button>
                             </div>
                         </div>
                     </>
 
-                    <div>
-                        <Link to="/login">
+                    <div className="Header-User-Menu-Login">
+                        <Link to="/login" className="Header-User-Menu-Login-Link">
                             로그인
                         </Link>
-                        <Link to="/signUp">
+                        <Link to="/signUp" className="Header-User-Menu-Login-Link">
                             회원가입
                         </Link>
                     </div>
                 </div>
             </div>
 
-            <nav>
-                <div>
-                    <div>
-                        <button>
+            <nav className="Header-Nav">
+                <div className="Header-Nav-Container">
+                    <div className="Header-Nav-Category">
+                        <button className="Header-Nav-Category-Button">
                             카테고리<span>▼</span>
                         </button>
-                        <div>
-                            <Link to="/">
+                        <div className="Header-Nav-Category-Dropdown">
+                            <Link to="/" className="Header-Nav-Category-Dropdown-Link">
                                 카테고리
                             </Link>
                         </div>
                     </div>
 
-                    <Link to="/">
+                    <Link to="/" className="Header-Nav-Link">
                         전체상품
                     </Link>
-                    <Link to="/">
+                    <Link to="/" className="Header-Nav-Link">
                         인기상품
                     </Link>
-                    <Link to="/">
+                    <Link to="/" className="Header-Nav-Link">
                         신상품
                     </Link>
                 </div>
